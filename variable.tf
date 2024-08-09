34d2c1bd14b099596085cccc9b0e3546dc4e2bd6
bertechaws <Cherazarbernadin@gmail.com>
2024-08-09 08:45:01 -0400

    created security group file

--- variable.tf
+++ variable.tf
@@ -48,3 +48,11 @@ variable "private_data__subnet_az2_cidr" {
   description = "private data subnet az2 cidr block"
   type = string
 }
+
+#Security groups variables
+variable "ssh_location" {
+  default = "97.100.233.204/32"
+
+  description = "the ip address that can ssh into the ec2 instance"
+  type = string
+}
